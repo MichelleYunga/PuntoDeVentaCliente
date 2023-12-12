19c2fc5a74a22d959cb3b0a5a5d1a2256bd8c4aa
MichelleYunga <michelle.yunga.est@tecazuay.edu.ec>
2023-12-12 10:03:09 -0500

    Cliente Punto de venta

---
+++ src/java/Vista/Inicio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Vista;
+
+/**
+ *
+ * @author andre
+ */
+public class Inicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu1 menu = new Menu1();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+    
+}
